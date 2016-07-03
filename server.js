cf18de5559ed166254b46d8c7157fee73fc8fc42
tellingeric <tellingeric@hotmail.com>
2016-07-02 21:18:19 -0400

    CORS fixed

--- server.js
+++ server.js
@@ -26,6 +26,19 @@ app.set('secretCode', config.secret);
 // all of our routes will be prefixed with /api
 app.use('/', require('./app/routes'));
 
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 
 // =============================================================================
 app.set('port', process.env.PORT || 3000);
