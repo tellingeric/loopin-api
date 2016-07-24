55d7003fb99ef2fa7e6bd211aa1493ac849330ff
zhisheng zhou <zz676@nyu.edu>
2016-07-23 17:38:27 -0700

    Remove database.js under config folder

--- app/config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-    'secret': 'loopin-api',
-    'database': 'mongodb://sam003:admin@ds040349.mlab.com:40349/yummyt'
-};
--- app/config/passport.js
+++ app/config/passport.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/UserModel');
-const config = require('../config/database');
+const config = require('./../../config');
 
 // Setup work and export for the JWT passport strategy
 module.exports = function(passport) {
