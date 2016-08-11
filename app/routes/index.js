2b35a3e2f60272f93a694cbd49e944b39e3e50ba
Wenwei <caowenwei@gmail.com>
2016-08-11 06:44:44 -0400

    change all update route http verb to PUT

--- app/routes/index.js
+++ app/routes/index.js
@@ -23,21 +23,21 @@ router.get('/api/me', jsonParser, Users.getUser);
 router.get('/api/vendors', Vendors.getAll);
 router.post('/api/vendors', Vendors.createOne);
 router.get('/api/vendors/:vendor_id', Vendors.getOne);
-router.post('/api/vendors/:vendor_id', Vendors.updateOne);
+router.put('/api/vendors/:vendor_id', Vendors.updateOne);
 router.delete('/api/vendors/:vendor_id', Vendors.deleteOne);
 
 // PRODUCTS
 router.get('/api/products', Products.getAll);
 router.post('/api/products', Products.createOne);
 router.get('/api/products/:product_id', Products.getOne);
-router.post('/api/products/:product_id', Products.updateOne);
+router.put('/api/products/:product_id', Products.updateOne);
 router.delete('/api/products/:product_id', Products.deleteOne);
 
 // EVENTS
 router.get('/api/events', Events.getAll);
 router.post('/api/events', Events.createOne);
 router.get('/api/events/:event_id', Events.getOne);
-router.post('/api/events/:event_id', Events.updateOne);
+router.put('/api/events/:event_id', Events.updateOne);
 router.delete('/api/events/:event_id', Events.deleteOne);
 
 
@@ -45,7 +45,7 @@ router.delete('/api/events/:event_id', Events.deleteOne);
 router.get('/api/orders', Orders.getAll);
 router.post('/api/orders', Orders.createOne);
 router.get('/api/orders/:order_id', Orders.getOne);
-router.post('/api/orders/:order_id', Orders.updateOne);
+router.put('/api/orders/:order_id', Orders.updateOne);
 router.delete('/api/orders/:order_id', Orders.deleteOne);
 
 //DEBUG ROUTES, MUST REMOVE IN PRODUCTION
