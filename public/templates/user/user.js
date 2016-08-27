9f54a6c896bda5731b7e9359bd1bc81d3ef091e7
tellingeric <tellingeric@hotmail.com>
2016-08-27 00:00:13 -0400

    added pages

---
+++ public/templates/user/user.js
@@ -0,0 +1,21 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "/login";
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        myFunction(myArr);
+    }
+};
+xmlhttp.open("POST", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<a href="' + arr[i].url + '">' +
+        arr[i].display + '</a><br>';
+    }
+    document.getElementById("id01").innerHTML = out;
+}
