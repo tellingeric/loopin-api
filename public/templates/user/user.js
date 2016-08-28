a497a53192c4a18f34063cb7f006193011025755
tellingeric <tellingeric@hotmail.com>
2016-08-27 23:55:47 -0400

    update templates for dash

--- public/templates/user/user.js
+++ public/templates/user/user.js
@@ -4,18 +4,9 @@ var url = "/login";
 xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var myArr = JSON.parse(xmlhttp.responseText);
-        myFunction(myArr);
     }
 };
-xmlhttp.open("POST", url, true);
-xmlhttp.send();
 
-function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' +
-        arr[i].display + '</a><br>';
-    }
-    document.getElementById("id01").innerHTML = out;
-}
+xmlhttp.open("POST", "/login", true);
+xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xmlhttp.send();
