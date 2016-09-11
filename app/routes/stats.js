ee6cfee111b6332f0e6b2654a7910773b2fbb587
Wenwei <caowenwei@gmail.com>
2016-09-11 16:57:06 -0400

    add stats

--- app/routes/stats.js
+++ app/routes/stats.js
@@ -1,13 +1,40 @@
 var UserModel = require('../models/UserModel');
+var VendorModel = require('../models/VendorModel');
+var ProductModel = require('../models/ProductModel');
+var EventModel = require('../models/EventModel');
+var OrderModel = require('../models/OrderModel');
+var AssetModel = require('../models/AssetModel');
 
 var Stats = {
 
     getStats: function(req, res) {
 
-        UserModel.count({}, function(err, count){
-            if (err) res.send(err);
-            res.json({numOfUsers: count});
-        });
+        var stats = {};
+        UserModel.count({}).exec()
+            .then(function(num){
+                stats.numOfUsers = num;
+                return VendorModel.count({}).exec();
+            })
+            .then(function(num){
+                stats.numOfVendors = num;
+                return ProductModel.count({}).exec();
+            })
+            .then(function(num){
+                stats.numOfProducts = num;
+                return EventModel.count({}).exec();
+            })
+            .then(function(num){
+                stats.numOfEvents = num;
+                return OrderModel.count({}).exec();
+            })
+            .then(function(num){
+                stats.numOfOrders = num;
+                return AssetModel.count({}).exec();
+            })
+            .then(function(num){
+                stats.numOfAssets = num;
+                res.json(stats);
+            })
 
     }
 
