c69f84e38cde4ff92679425efbece0b1f2a26a45
zhisheng zhou <zz676@nyu.edu>
2016-09-17 11:56:16 -0700

    change the databse back

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'loopIn-api',
-    'database': 'mongodb://sam003:admin@ds040349.mlab.com:40349/yummyt'// Sam's Database
-    //'database': 'mongodb://admin:admin@ds021694.mlab.com:21694/yummyt-test'
+    //'database': 'mongodb://sam003:admin@ds040349.mlab.com:40349/yummyt'// Sam's Database
+    'database': 'mongodb://admin:admin@ds021694.mlab.com:21694/yummyt-test'
 };
